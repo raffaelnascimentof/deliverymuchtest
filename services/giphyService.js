a66b68735b22d2b68a17b50959c789725a1df19b
Raffael Ferreira <raffaelnascimentof@gmail.com>
2020-07-28 13:52:11 -0300

    Finzalizar projeto

--- services/giphyService.js
+++ services/giphyService.js
@@ -1,7 +1,9 @@
 const axios = require('axios')
 
 async function getGiphy (nameOfRecipe) {
-  return axios.get(`http://api.giphy.com/v1/gifs/search?q=${nameOfRecipe}&api_key=zupVZrSHHlZsh0i1W9G8ZFluaTElB3B8&limit=1`)
+  const API_KEY = ''
+
+  return axios.get(`http://api.giphy.com/v1/gifs/search?q=${nameOfRecipe}&api_key=${API_KEY}&limit=1`)
 }
 
 module.exports = {
